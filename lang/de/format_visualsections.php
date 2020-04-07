351972aa50a683b10314913798c2518baaa0a504
gthomas2 <brudinie@gmail.com>
2020-04-07 09:18:24 +0100

    Added DE (Deutsch) language file

---
+++ lang/de/format_visualsections.php
@@ -0,0 +1,74 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'format_visualsections', language 'de_ch', branch 'MOODLE_20_STABLE'
+ *
+ * @package   format_visualsections
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['addsections'] = 'Visualsections hinzufügen';
+$string['addsubsection'] = 'Unterabschnitt hinzufügen';
+$string['currentsection'] = 'Dieser Abschnitt';
+$string['editsection'] = 'Abschnitt bearbeiten';
+$string['editsectionname'] = 'Abschnittsnamen bearbeiten';
+$string['deletesection'] = 'Abschnitt löschen';
+$string['newsectionname'] = 'Neuer Name für Abschnitt {$a}';
+$string['sectionname'] = 'Abschnitt';
+$string['pluginname'] = 'Visual Section-Format';
+$string['section0name'] = 'Allgemein';
+$string['page-course-view-visualsections'] = 'Jede Kurshauptseite im Visualsection-Format';
+$string['page-course-view-visualsections-x'] = 'Jede Kursseite im Visual Section-Format';
+$string['hidefromothers'] = 'Abschnitt verbergen';
+$string['showfromothers'] = 'Abschnitt anzeigen';
+$string['privacy:metadata'] = 'Das Plugin Kursformat Format Visual Sections speichert keine persönlichen Daten.';
+$string['subsectioncreated'] = 'Unterabschnitt erstellt';
+$string['subsectioncreatefailed'] = 'Unterabschnitt nicht erstellt';
+$string['size:s'] = 'Klein';
+$string['size:m'] = 'Medium';
+$string['size:l'] = 'Gross';
+$string['definesubsection'] = 'Unterabschnitt-Details';
+$string['subsectiontype'] = 'Unterabschnitt-Typ';
+$string['subsectionname'] = 'Unterabschnitt-Name';
+$string['subsectionsize'] = 'Unterabschnitt-Grösse';
+$string['subsectiontypes'] = 'Unterabschnitt-Typen';
+$string['subsectiontypes_desc'] = 'Fügen Sie fünf Unterabschnitts-Typen hinzu. Jeder Typ erfordert einen Code, einen Namen und ein Bild.';
+$string['subsectioncreated'] = 'Unterabschnitt erstellt';
+$string['movesubsectionup'] = 'Unterabschnitt nach oben verschieben';
+$string['movesubsectiondown'] = 'Unterabschnitt nach unten verschieben';
+$string['deletesubsection'] = 'Unterabschnitt löschen';
+$string['code'] = 'Code';
+$string['name'] = 'Name';
+$string['addsubsectiontype'] = 'Neuer Unterabschnitt-Typ hinzufügen';
+$string['removesubsectiontype'] = 'Unterabschnitt-Typ "{$a}" entfernen';
+$string['addimagetosubsectiontype'] = 'Bild für Unterabschnitt-Typ hinzufügen';
+$string['editsubsection'] = 'Unterabschnitt bearbeiten';
+$string['addsubsection'] = 'Unterabschnitt hinzufügen';
+$string['sectionlocked'] = 'Dieser Abschnitt ist gesperrt. Sie müssen den vorherigen Abschnitt abschliessen, bevor Sie darauf zugreifen können.';
+$string['label:parentid'] = 'Eltern ID';
+$string['label:typecode'] = 'Type Code';
+$string['label:size'] = 'Grösse';
+$string['navigatetosection'] = 'Gehe zu Abschnitt {$a}';
+$string['previoussections'] = 'Vorheriger Gruppe von Abschnitten';
+$string['nextsections'] = 'Nächste Gruppe von Abschnitten';
+$string['prevsection'] = 'Vorheriger Abschnitt';
+$string['nextsection'] = 'Nächster Abschnitt';
+$string['nextsectionlocked'] = 'Auf den nächsten Abschnitt kann erst zugegriffen werden, wenn der aktuelle Abschnitt abgeschlossen ist.';
+$string['toomanysubsections'] = 'Zu viele Unterabschnitte - max. 5';
+$string['failedtomovesubsection'] = 'Unterabschnitt nicht verschoben';
