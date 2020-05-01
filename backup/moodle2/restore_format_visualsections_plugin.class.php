e25c79802aa53bc37dd0fce0b3dc0883d12f99e9
Guy Thomas <dev@citri.city>
2020-05-01 18:00:33 +0100

    Fix course restore

--- backup/moodle2/restore_format_visualsections_plugin.class.php
+++ backup/moodle2/restore_format_visualsections_plugin.class.php
@@ -32,55 +32,10 @@ defined('MOODLE_INTERNAL') || die();
  *
  * @package   format_visualsections
  * @category  backup
- * @copyright 2017 Marina Glancy
+ * @copyright 2020 Citricity Ltd
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class restore_format_visualsections_plugin extends restore_format_plugin {
-
-    /** @var int */
-    protected $originalnumsections = 0;
-
-    /**
-     * Checks if backup file was made on Moodle before 3.3 and we should respect the 'numsections'
-     * and potential "orphaned" sections in the end of the course.
-     *
-     * @return bool
-     */
-    protected function need_restore_numsections() {
-        $backupinfo = $this->step->get_task()->get_info();
-        $backuprelease = $backupinfo->backup_release;
-        return version_compare($backuprelease, '3.3', 'lt');
-    }
-
-    /**
-     * Creates a dummy path element in order to be able to execute code after restore
-     *
-     * @return restore_path_element[]
-     */
-    public function define_course_plugin_structure() {
-        global $DB;
-
-        // Since this method is executed before the restore we can do some pre-checks here.
-        // In case of merging backup into existing course find the current number of sections.
-        $target = $this->step->get_task()->get_target();
-        if (($target == backup::TARGET_CURRENT_ADDING || $target == backup::TARGET_EXISTING_ADDING) &&
-                $this->need_restore_numsections()) {
-            $maxsection = $DB->get_field_sql(
-                'SELECT max(section) FROM {course_sections} WHERE course = ?',
-                [$this->step->get_task()->get_courseid()]);
-            $this->originalnumsections = (int)$maxsection;
-        }
-
-        // Dummy path element is needed in order for after_restore_course() to be called.
-        return [new restore_path_element('dummy_course', $this->get_pathfor('/dummycourse'))];
-    }
-
-    /**
-     * Dummy process method
-     */
-    public function process_dummy_course() {
-
-    }
+class restore_format_visualsections_plugin extends restore_format_topics_plugin {
 
     /**
      * Executed after course restore is complete
@@ -90,31 +45,46 @@ class restore_format_visualsections_plugin extends restore_format_plugin {
     public function after_restore_course() {
         global $DB;
 
-        if (!$this->need_restore_numsections()) {
-            // Backup file was made in Moodle 3.3 or later, we don't need to process 'numsecitons'.
-            return;
-        }
-
-        $data = $this->connectionpoint->get_data();
         $backupinfo = $this->step->get_task()->get_info();
-        if ($backupinfo->original_course_format !== 'visualsections' || !isset($data['tags']['numsections'])) {
-            // Backup from another course format or backup file does not even have 'numsections'.
+        if ($backupinfo->original_course_format !== 'visualsections') {
+            // Backup from another course format or backup.
             return;
         }
 
-        $numsections = (int)$data['tags']['numsections'];
+        $courseid = $this->task->get_courseid();
+
+        // Get section numbers by original section ids.
+        $sectnumsbyid = [];
         foreach ($backupinfo->sections as $key => $section) {
-            // For each section from the backup file check if it was restored and if was "orphaned" in the original
-            // course and mark it as hidden. This will leave all activities in it visible and available just as it was
-            // in the original course.
-            // Exception is when we restore with merging and the course already had a section with this section number,
-            // in this case we don't modify the visibility.
-            if ($this->step->get_task()->get_setting_value($key . '_included')) {
-                $sectionnum = (int)$section->title;
-                if ($sectionnum > $numsections && $sectionnum > $this->originalnumsections) {
-                    $DB->execute("UPDATE {course_sections} SET visible = 0 WHERE course = ? AND section = ?",
-                        [$this->step->get_task()->get_courseid(), $sectionnum]);
-                }
+            $path = $this->task->get_basepath();
+            $sectionfile = $path.'/'.$section->directory.'/section.xml';
+            $sectel = new SimpleXMLElement(file_get_contents($sectionfile));
+            $atts = $sectel->attributes();
+            $id = (int) $atts['id'][0];
+            $numberx = $sectel->xpath('//number');
+            $sectionnumber = (int) $numberx[0];
+            $sectnumsbyid[$id] = $sectionnumber;
+        }
+
+        $format = \format_visualsections::instance($courseid);
+
+        $newsectionidsbynum = $DB->get_records_menu('course_sections',
+            ['course' => $courseid], '', 'section, id'
+        );
+
+        foreach ($sectnumsbyid as $id => $num) {
+            $formatoptions = $format->get_format_options($num);
+            if (!empty($formatoptions['parentid'])) {
+                $sectionid = $newsectionidsbynum[$num];
+                $oldparentid = $formatoptions['parentid'];
+                // Get parent section number for old parent section id.
+                $sectnum = $sectnumsbyid[$oldparentid];
+                // Get new section id.
+                $newparentid = $newsectionidsbynum[$sectnum];
+                // Update format with new parentid.
+                $formatoptions['parentid'] = $newparentid;
+                $formatoptions['id'] = $sectionid;
+                $format->update_section_format_options($formatoptions);
             }
         }
     }
